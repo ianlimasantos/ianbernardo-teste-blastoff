07a2eb5fb553ef844dc9c0228ed4fb04b8da21cd
ianlimasantos <99565712+ianlimasantos@users.noreply.github.com>
2022-06-15 04:38:44 -0300

    Delete script.js

--- Q11/script.js
+++
@@ -1,7 +0,0 @@
-function multiplicacao (num1,num2) {
-    let resultado = num1 * num2;
-    return resultado;
-}
-console.log(multiplicacao(4, 7));
-console.log (multiplicacao(5, 6));
-console.log (multiplicacao(3, 3));
